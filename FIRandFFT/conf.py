20cd0abffe9ace60f5b01564d2cfab3473057de8
Yusuke Yoshihara <yoshihara.yuusuke@gmail.com>
2016-10-09 22:43:11 +0900

    add new lines

--- FIRandFFT/conf.py
+++ FIRandFFT/conf.py
@@ -2,8 +2,19 @@ Win1 = ""
 Win2 = ""
 
 original = []
-Srate = 0
-duration = 0
+filtered = []
 
+frq = None
+filename = None
+i001 = None
+num_samples = None
+dialog = None
+minfrq = None
+nyquist = None
+duration = None
+Srate = None
 
-
+p1 = None
+p2 = None
+p3 = None
+p4 = None
