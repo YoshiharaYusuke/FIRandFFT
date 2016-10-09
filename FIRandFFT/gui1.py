349d1418089698c90c616de68f1b634a5ab907d8
Yusuke Yoshihara <yoshihara.yuusuke@gmail.com>
2016-10-10 02:05:07 +0900

    add new lines

--- FIRandFFT/gui1.py
+++ FIRandFFT/gui1.py
@@ -11,7 +11,6 @@ import pyqtgraph as pg
 import conf
 import gui2
 
-
 pg.setConfigOption('foreground', 'k')
 pg.setConfigOption('background', 'w')
 
@@ -26,7 +25,7 @@ class Ui_MainWindow(object):
         self.horizontalLayout.setObjectName("horizontalLayout")
         self.verticalLayout_graph = QtGui.QVBoxLayout()
         self.verticalLayout_graph.setObjectName("verticalLayout_graph")
-        self.graphicsView_signal =  pg.PlotWidget()
+        self.graphicsView_signal = pg.PlotWidget()
         self.graphicsView_signal.setObjectName("graphicsView_signal")
         self.verticalLayout_graph.addWidget(self.graphicsView_signal)
         self.graphicsView_spectrum = pg.PlotWidget()
@@ -88,11 +87,10 @@ class Ui_MainWindow(object):
         self.menubar.addAction(self.menuFile.menuAction())
         self.menubar.addAction(self.menuLicence.menuAction())
 
-
         self.dialog = QtGui.QFileDialog()
-        self.dialog.setModal(True)#
-        self.dialog.setDirectory(os.path.expanduser('~'))#
-        filters = ["csv comma-separated values(*.csv)", "text file(*.txt)", "tab tab-separated values(*.tsv)", "wav file(*.wav)", "All(*)"]#     file extention.
+        self.dialog.setModal(True)
+        self.dialog.setDirectory(os.path.expanduser('~'))
+        filters = ["csv comma-separated values(*.csv)", "text file(*.txt)", "tab tab-separated values(*.tsv)", "wav file(*.wav)", "All(*)"]  # File extention.
         self.dialog.setNameFilters(filters)
         QtCore.QObject.connect(self.dialog, QtCore.SIGNAL("accepted()"), self.load)
         QtCore.QObject.connect(self.dialog, QtCore.SIGNAL("rejected()"), self.reject)
@@ -101,22 +99,30 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
         QtCore.QObject.connect(self.actionOpen, QtCore.SIGNAL("triggered()"), self.open)
-        QtCore.QObject.connect(self.actionSample, QtCore.SIGNAL("triggered()"), MainWindow.close)
+        QtCore.QObject.connect(self.actionSample, QtCore.SIGNAL("triggered()"), self.sample)
         QtCore.QObject.connect(self.comboBox_filter, QtCore.SIGNAL("currentIndexChanged(int)"), self.rePlot)
         QtCore.QObject.connect(self.doubleSpinBox_high, QtCore.SIGNAL("editingFinished()"), self.cutoff)
         QtCore.QObject.connect(self.doubleSpinBox_low, QtCore.SIGNAL("editingFinished()"), self.cutoff)
         QtCore.QObject.connect(self.spinBox_tap, QtCore.SIGNAL("editingFinished()"), self.tap)
-        QtCore.QObject.connect(self.actionShow, QtCore.SIGNAL("triggered()"), self.License)#
+        QtCore.QObject.connect(self.actionShow, QtCore.SIGNAL("triggered()"), self.License)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-        self.spinBox_tap.setProperty("value", 101)#
+        self.spinBox_tap.setProperty("value", 101)
         self.spinBox_tap.setMinimum(1)
         self.spinBox_tap.setMaximum(9999999)
         self.spinBox_tap.setSingleStep(2)
 
-        self.graphicsView_signal.addLegend(bkgnd=(0,0,0,0),frame=(0,0,0,255))
-        self.graphicsView_spectrum.addLegend(bkgnd=(0,0,0,0),frame=(0,0,0,255))
+        self.graphicsView_signal.addLegend(bkgnd=(0, 0, 0, 0), frame=(0, 0, 0, 255))
+        self.graphicsView_spectrum.addLegend(bkgnd=(0, 0, 0, 0), frame=(0, 0, 0, 255))
 
+        labelStyle = {'color': 'B', 'font-size': '14pt', 'font-family': 'Arial'}
+
+        self.graphicsView_signal.setLabel("bottom", text="x", **labelStyle)
+        self.graphicsView_signal.setLabel("left", text="y", **labelStyle)
+        self.graphicsView_signal.addLine(y=0, pen=pg.mkPen([0, 0, 0]))  # Line at origin
+
+        self.graphicsView_spectrum.setLabel("bottom", text="Frequency f", units="Hz",  **labelStyle)
+        self.graphicsView_spectrum.setLabel("left", text="Amplitude", **labelStyle)
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "FIRandFFT", None, QtGui.QApplication.UnicodeUTF8))
@@ -150,16 +156,20 @@ class Ui_MainWindow(object):
             conf.Srate = wr.getframerate()
             conf.duration = float(wr.getnframes()) * 2.0 / float(wr.getframerate())
             data = wr.readframes(wr.getnframes())
-            num_data = fromstring(data, dtype = int16)
+            num_data = fromstring(data, dtype=int16)
             conf.original = np.linspace(0, conf.duration, wr.getnframes() * 2)
-            conf.original = np.vstack((conf.original, np.r_[num_data[::2],num_data[1::2]]))
+            conf.original = np.vstack((conf.original, np.r_[num_data[::2], num_data[1::2]]))
 
         elif self.dialog.selectedFilter() == "tsv tab-separated values(*.tsv)":
             conf.original = np.loadtxt(unicode(self.dialog.selectedFiles()[0]), delimiter='\t').T
 
         else:
             conf.original = np.loadtxt(unicode(self.dialog.selectedFiles()[0]), delimiter=',').T
-        
+
+        self.start()
+
+    def start(self):
+        self.clearPlot()
         self.setProperty()
         self.plotOriginal()
         self.filter()
@@ -176,11 +186,31 @@ class Ui_MainWindow(object):
         self.doubleSpinBox_high.setValue(conf.nyquist/1.1)
         self.doubleSpinBox_low.setValue(conf.minfrq*1.1)
         self.doubleSpinBox_high.setSingleStep(conf.minfrq)
-        self.doubleSpinBox_low.setSingleStep(conf.minfrq)        
+        self.doubleSpinBox_low.setSingleStep(conf.minfrq)
         self.spinBox_tap.setMaximum(conf.num_samples)
 
     def reject(self):
         pass
+
+    def sample(self):
+        num = 10001
+        frq1 = 1
+        frq2 = 10
+        frq3 = 100
+        frq4 = 1000
+        theta1 = np.linspace(0, 2*np.pi*frq1, num)
+        theta2 = np.linspace(0, 2*np.pi*frq2, num)
+        theta3 = np.linspace(0, 2*np.pi*frq3, num)
+        theta4 = np.linspace(0, 2*np.pi*frq4, num)
+        sin1 = np.sin(theta1)
+        sin2 = np.sin(theta2)
+        sin3 = np.sin(theta3)
+        sin4 = np.sin(theta4)
+
+        x = np.linspace(0, 1, num)
+        y = sin1 + sin2 + sin3 + sin4
+        conf.original = np.vstack((x, y))
+        self.start()
 
     def open(self):
         self.dialog.show()
@@ -196,18 +226,18 @@ class Ui_MainWindow(object):
         frq = np.linspace(0, conf.nyquist, int(np.floor(conf.num_samples / 2.0))) + conf.minfrq / 10.0
         spectrum = abs(np.fft.fft(conf.original[1])[0:int(np.floor(conf.num_samples / 2.0))]) / len(conf.original[1]) * 2.0
         conf.p2 = self.graphicsView_spectrum.plot(frq, spectrum, name="FFT spectrum of the original signal")
-        
+
     def plotFiltered(self):
-        conf.p3 = self.graphicsView_signal.plot(conf.original[0], conf.filtered, name="Filtered signal", pen=[250,0,0,100])
+        conf.p3 = self.graphicsView_signal.plot(conf.original[0], conf.filtered, name="Filtered signal", pen=[250, 0, 0, 100])
         frq = np.linspace(0, conf.nyquist, int(np.floor(conf.num_samples / 2.0))) + conf.minfrq / 10.0
         spectrum = abs(np.fft.fft(conf.filtered)[0:int(np.floor(conf.num_samples / 2.0))]) / len(conf.original[1]) * 2.0
-        conf.p4 = self.graphicsView_spectrum.plot(frq, spectrum, name="FFT spectrum of the filtered signal", pen=[250,0,0,100])
+        conf.p4 = self.graphicsView_spectrum.plot(frq, spectrum, name="FFT spectrum of the filtered signal", pen=[250, 0, 0, 100])
 
     def clearPlot(self):
         self.graphicsView_signal.clear()
         self.graphicsView_spectrum.clear()
-        
-        #self.graphicsView_signal.removeItem(self.graphicsView_signal)
+
+        self.clearLegend()
 
     def clearPlotFiltered(self):
         self.graphicsView_signal.removeItem(conf.p1)
@@ -215,23 +245,26 @@ class Ui_MainWindow(object):
         self.graphicsView_signal.removeItem(conf.p3)
         self.graphicsView_spectrum.removeItem(conf.p4)
 
+        self.clearLegend()
+
+    def clearLegend(self):
         self.graphicsView_signal.plotItem.legend.removeItem("Original signal")
         self.graphicsView_signal.plotItem.legend.removeItem("Filtered signal")
         self.graphicsView_spectrum.plotItem.legend.removeItem("FFT spectrum of the original signal")
         self.graphicsView_spectrum.plotItem.legend.removeItem("FFT spectrum of the filtered signal")
 
     def filter(self):
-        if self.comboBox_filter.currentIndex() == 0:#LP
-            filtre = np.array(scsi.firwin(self.spinBox_tap.value(), self.doubleSpinBox_high.value(), nyq = conf.nyquist))
-        elif self.comboBox_filter.currentIndex() == 1:#HP
-            filtre = np.array(scsi.firwin(self.spinBox_tap.value(), self.doubleSpinBox_high.value(), pass_zero = False, nyq = conf.nyquist))
-        elif self.comboBox_filter.currentIndex() == 2:#BP
-            filtre = np.array(scsi.firwin(self.spinBox_tap.value(), [self.doubleSpinBox_low.value(), self.doubleSpinBox_high.value()], pass_zero = False, nyq = conf.nyquist))
-        elif self.comboBox_filter.currentIndex() == 3:#BS
-            filtre = np.array(scsi.firwin(self.spinBox_tap.value(), [self.doubleSpinBox_low.value(), self.doubleSpinBox_high.value()], nyq = conf.nyquist))
+        if self.comboBox_filter.currentIndex() == 0:  # LP
+            filtre = np.array(scsi.firwin(self.spinBox_tap.value(), self.doubleSpinBox_high.value(), nyq=conf.nyquist))
+        elif self.comboBox_filter.currentIndex() == 1:  # HP
+            filtre = np.array(scsi.firwin(self.spinBox_tap.value(), self.doubleSpinBox_high.value(), pass_zero=False, nyq=conf.nyquist))
+        elif self.comboBox_filter.currentIndex() == 2:  # BP
+            filtre = np.array(scsi.firwin(self.spinBox_tap.value(), [self.doubleSpinBox_low.value(), self.doubleSpinBox_high.value()], pass_zero=False, nyq=conf.nyquist))
+        elif self.comboBox_filter.currentIndex() == 3:  # BS
+            filtre = np.array(scsi.firwin(self.spinBox_tap.value(), [self.doubleSpinBox_low.value(), self.doubleSpinBox_high.value()], nyq=conf.nyquist))
         else:
             pass
-        conf.filtered = np.convolve(conf.original[1], filtre, mode = 'same')
+        conf.filtered = np.convolve(conf.original[1], filtre, mode='same')
 
     def setInitialRegion(self):
         self.doubleSpinBox_low.setMinimum(conf.minfrq)
@@ -239,20 +272,20 @@ class Ui_MainWindow(object):
         self.region = pg.LinearRegionItem(bounds=[self.doubleSpinBox_low.value(), self.doubleSpinBox_high.value()])
         self.region.setZValue(10)
         self.region.setBounds([np.log10(conf.minfrq), np.log10(conf.nyquist-conf.minfrq)])
-        self.region.setRegion([np.log10(self.doubleSpinBox_low.value()), np.log10(self.doubleSpinBox_high.value())])#log 1 means e+01
+        self.region.setRegion([np.log10(self.doubleSpinBox_low.value()), np.log10(self.doubleSpinBox_high.value())])
         self.region.sigRegionChanged.connect(self.regionRange)
         self.region.sigRegionChangeFinished.connect(self.regionRangeAndCal)
         self.midPoints()
-        self.graphicsView_spectrum.addItem(self.region)#, ignoreBounds=True)
+        self.graphicsView_spectrum.addItem(self.region)
 
     def regionRange(self):
-        v1, v2=self.region.getRegion()
+        v1, v2 = self.region.getRegion()
         self.doubleSpinBox_low.setValue(10**v1)
         self.doubleSpinBox_high.setValue(10**v2)
         self.midPoints()
 
     def regionRangeAndCal(self):
-        v1, v2=self.region.getRegion()
+        v1, v2 = self.region.getRegion()
         self.doubleSpinBox_low.setValue(10**v1)
         self.doubleSpinBox_high.setValue(10**v2)
         self.midPoints()
@@ -267,11 +300,10 @@ class Ui_MainWindow(object):
         self.plotOriginal()
         self.filter()
         self.plotFiltered()
-        
+
     def setRegion(self):
         v1 = np.log10(self.doubleSpinBox_low.value())
         v2 = np.log10(self.doubleSpinBox_high.value())
-        print(v1+v2)
         self.region.setRegion((v1, v2))
         self.midPoints()
 
